159edac944e0e5114a53e9c036c369cb3df94386
Philip Graham <GrahPhil963@GOTVDSB.CA>
2022-03-21 10:19:34 -0400

    Add project files.

---
+++ Part 4 Assignment PHILIP GRAHAM/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Part_4_Assignment_PHILIP_GRAHAM
+{
+    public partial class RandomNumbers : Form
+    {
+        public RandomNumbers()
+        {
+            InitializeComponent();
+        }
+    }
+}
